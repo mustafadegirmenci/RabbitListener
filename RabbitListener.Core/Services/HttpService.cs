b6856f2dbdaec1ccd5e644c66c94a933c146dd32
mustafadegirmenci <88446086+mustafadegirmenci@users.noreply.github.com>
2023-07-03 16:51:39 +0300

    Handle invalid operation exception (http service)

--- RabbitListener.Core/Services/HttpService.cs
+++ RabbitListener.Core/Services/HttpService.cs
@@ -2,20 +2,24 @@
 
 public class HttpService
 {
-public async Task<int> GetUrlResponseStatusCodeAsync(string url)
-{
-    try
+    public async Task<int> GetUrlResponseStatusCodeAsync(string url)
     {
-        using var client = new HttpClient();
-        var request = new HttpRequestMessage(HttpMethod.Head, url);
-        var response = await client.SendAsync(request);
-        
-        return (int)response.StatusCode;
+        try
+        {
+            using var client = new HttpClient();
+            var request = new HttpRequestMessage(HttpMethod.Head, url);
+            var response = await client.SendAsync(request);
+            
+            return (int)response.StatusCode;
+        }
+        catch (HttpRequestException _)
+        {
+            return -1;
+        }
+        catch (InvalidOperationException _)
+        {
+            return -1;
+        }
     }
-    catch (HttpRequestException _)
-    {
-        return -1;
-    }
-}
 
 }
